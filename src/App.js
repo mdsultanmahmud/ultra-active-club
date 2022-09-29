0665857a3ebfcefb2779a303c6dc081b83b6be90
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-09-29 19:04:56 +0600

    added element part done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <Header></Header>
         <div className='product-part'>
           <Element></Element>
+          <AddedElement></AddedElement>
         </div>
     </div>
   );
