3063abbbfeaf300fab160c9ccb668886abd7c7bb
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-09-29 19:04:21 +0600

    added element file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className='App.css'>
         <Header></Header>
-       
+        <div className='product-part'>
+          <Element></Element>
+        </div>
     </div>
   );
 }
